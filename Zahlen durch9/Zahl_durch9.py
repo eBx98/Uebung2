c67d1d5ebd8845e7fc964705e5947fd6da70d79b
eBx98 <erik.brinker@web.de>
2021-11-12 13:08:01 +0100

    Uebung2.1

---
+++ Zahlen durch9/Zahl_durch9.py
@@ -0,0 +1,9 @@
+Zahl_durch_neun = []
+
+for i in range(100,200):
+    if i %9 == 0:
+        Zahl_durch_neun.append(i)
+
+for neun in Zahl_durch_neun:
+    print(neun)
+        
---
+++ Zinssatz/Zinssatz.py
@@ -0,0 +1,8 @@
+#Sie bauen ein Haus und wollen wissen wieviel Geld Sie bei einer bestimmten Summe
+#und einem bestimmten Zinssatz monatlich zahlen müssen, wenn Sie in 10 Jahren schuldenfrei sein wollen.
+
+Kosten = 250000
+Zins = 2
+Jahre = 10
+Monate = 12
+
