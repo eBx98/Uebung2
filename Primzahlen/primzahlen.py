b9ab613da456fbcfd82f7d9b60b05f3b7ec4c33a
eBx98 <erik.brinker@web.de>
2021-11-17 14:38:34 +0100

    Uebung2

--- Primzahlen/primzahlen.py
+++ Primzahlen/primzahlen.py
@@ -9,16 +9,16 @@
 
 
 
-prim = []                               #Liste für die Primzahlen erstellt
+primzahlen = []                               #Liste für die Primzahlen erstellt
 
-for num in range(2,101):                #Bereich der Zahlen angeben aus denen die Primzahlen ermittelt werden sollen
-    prime = True                        #Für alle Nummern in dem Bereich ist die Variable prime --->immer<--- als true markiert(kann nur durch false geändert werden)
-    for i in range(2,num):              #Neuer Bereich der Zahlen, die unter der Zahl liegen, die ich als Primzahl ermitteln will
-        if (num%i==0):                  #Ist meine Primzahl durch eine Zahl in dem oberen Bereich (i) teilbar,
-            prime = False               #ist es keien Primzahl, prime wird dadurch auf false gesetzt
+for Zahl in range(2,101):                #Bereich der Zahlen angeben aus denen die Primzahlen ermittelt werden sollen
+    prim = True                        #Für alle Nummern in dem Bereich ist die Variable prime --->immer<--- als true markiert(kann nur durch false geändert werden)
+    for i in range(2,Zahl):              #Neuer Bereich der Zahlen, die unter der Zahl liegen, die ich als Primzahl ermitteln will
+        if (Zahl%i==0):                  #Ist meine Primzahl durch eine Zahl in dem oberen Bereich (i) teilbar,
+            prim = False               #ist es keien Primzahl, prime wird dadurch auf false gesetzt
 
-    if prime:                           #für alle Zahlen, bei denen prime true ist, 
-       prim.append(num)                 #werden diese hier in die Liste der PRimzahlen gepackt
+    if prim:                           #für alle Zahlen, bei denen prime true ist, 
+       primzahlen.append(Zahl)                 #werden diese hier in die Liste der PRimzahlen gepackt
 
-for pr in prim:                         #Liste ausgeben
+for pr in primzahlen:                         #Liste ausgeben
     print(pr)
